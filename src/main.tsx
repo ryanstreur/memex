9c4ea8c74fa731becb4d88be6004f43452db31e2
Ryan Streur <ryan@ryanstreur.com>
2023-08-08 12:22:37 -0700

    add material UI library

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import ReactDOM from "react-dom/client";
 
 import { initializeLocalStorage } from "./persistence.ts";
 import "./index.css";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import App from "./App.tsx";
 
 initializeLocalStorage();
